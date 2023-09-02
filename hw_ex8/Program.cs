76fe36e79b18bc57f527497b651f1d25b1692e31
Nurzhan Qusainkan <qusainkann@ggmail.com>
2023-09-02 18:22:27 +0600

    Домашнее задание: Задача 8

---
+++ hw_ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число N: ");
+string strNum = Console.ReadLine();
+int N = int.Parse(strNum);
+
+Console.WriteLine("Четные числа от 1 до N: ");
+
+int i = 2;
+while (i <= N)
+{
+    System.Console.WriteLine(i);
+    i += 2;
+}
