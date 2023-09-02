2021c5a80fb25d0502810744cc73f0c5a2739771
Nurzhan Qusainkan <qusainkann@ggmail.com>
2023-09-02 17:29:11 +0600

    Ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter number : ");
+string strNum = Console.ReadLine();
+int num1 = int.Parse(strNum);
+
+Console.Write("Enter number : ");
+strNum = Console.ReadLine();
+int num2 = int.Parse(strNum);
+
+int sqr = num2 * num2;
+
+if (sqr == num1)
+{
+    System.Console.WriteLine("Yes");
+}
+
+else
+{
+    System.Console.WriteLine("No");
+}
